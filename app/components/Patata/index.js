1d4c5a12ef2abf2ed8f63d69ac06fa6aa23377d1
adai183 <andreas.daiminger@gmail.com>
2016-07-28 21:12:14 +0200

    chore: setup plop generator && refactor component template

---
+++ app/components/Patata/index.js
@@ -0,0 +1,20 @@
+/**
+*
+* Patata
+*
+*/
+
+import React from 'react';
+
+import styles from './styles.css';
+
+class Patata extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div className={styles.patata}>
+      </div>
+    );
+  }
+}
+
+export default Patata;
